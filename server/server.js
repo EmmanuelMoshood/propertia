2994062b01b83cf619997885bfbfec6729c230d2
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-08-02 15:16:07 -0400

    connect to db

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import {DATABASE} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+
+// setup db
+mongoose.set("strictQuery", false); // to fix depreciation warning 
+mongoose
+.connect(DATABASE)
+.then(() => console.log("db is connected"))
+.catch((error) => console.log(`error in connecting to db: "${error}`));
 
 // middlewares
 app.use(express.json());
