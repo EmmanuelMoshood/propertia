0bcc795f18f0712812b7b1eb82f7904b0f919499
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-08-10 10:03:47 -0400

    got styling theme for ui from bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './bootstrap.css';
+// import '../src/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="">
+       <h1 className= "display-1 bg-primary text-light" >Hello React</h1>
       </header>
     </div>
   );
