a6483a5f30b4625822eada7ed7f02c0271a48860
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-08-12 07:26:38 -0400

    connect to server using axios http client

--- client/src/pages/Login_page.js
+++ client/src/pages/Login_page.js
@@ -10,11 +10,12 @@ export default function Login() {
             <div className="form-group">
               <label for="exampleInputEmail1" className="form-label mt-4">Email address</label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" fdprocessedid="cz21oq" />
-              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              
             </div>
             <div className="form-group">
               <label for="exampleInputPassword1" className="form-label mt-4">Password</label>
               <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" autocomplete="off" fdprocessedid="o2rlil" />
+              <small id="passwordHelp" className="form-text text-muted">If you can't seem to remember your password, click <a>forgot password</a></small>
             </div>
       
  
--- client/src/pages/Register_page.js
+++ client/src/pages/Register_page.js
@@ -1,18 +1,37 @@
-// pages/Home.js
+// store user input in a component
+import { useState } from "react";
+
+// hppt client
+import axios from "axios";
+
+//API
+import {API} from "../config"
+
+
 
 //components
-import UserStatus from "../components/UserStatus"
-import LikesButton from "../components/LikesButton";
+// import UserStatus from "../components/UserStatus"
+// import LikesButton from "../components/LikesButton";
 
 
 
 export default function Register() {
+    //state
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleSubmit = async () => {
+    //
+    const handleSubmit = async (e) => {
         try{
-            
-        }catch{
+            e.preventDefault();
+            // console.table({ email, password })
 
+            const res = await axios.post(`${API}/pre-register`, {email, password}) 
+            const {data} = res;
+            console.log(data)
+
+        }catch(err){
+            console.log(err)
         }
     }
  
@@ -31,11 +50,29 @@ export default function Register() {
             
             <div className="form-group">
               <label for="exampleInputEmail1" className="form-label mt-4">Email address</label>
-              <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter email" fdprocessedid="cz21oq" />
+              <input 
+                type="email" 
+                className="form-control" 
+                id="userEmail" 
+                aria-describedby="emailHelp" 
+                placeholder="Enter email" 
+                fdprocessedid="cz21oq"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="form-group">
               <label for="exampleInputPassword1" className="form-label mt-4">Password</label>
-              <input type="password" className="form-control" id="userPassword" placeholder="Password" autocomplete="off" fdprocessedid="o2rlil" />
+              <input 
+              type="password" 
+              className="form-control" 
+              id="userPassword" 
+              placeholder="Password" 
+              autocomplete="off" 
+              fdprocessedid="o2rlil" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
       
  
