96d2ab5fcde82c90edfa623bbbb34925ecbd0ed2
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-08-11 22:36:42 -0400

    registration form

--- client/src/pages/Register_page.js
+++ client/src/pages/Register_page.js
@@ -7,11 +7,41 @@ import LikesButton from "../components/LikesButton";
 
 
 export default function Register() {
+
+    const handleSubmit = async () => {
+        try{
+            
+        }catch{
+
+        }
+    }
+ 
+
+
     return (
       <div>
-        <h1 className="display-1 bg-primary text-light p-5">Register</h1>
-        <UserStatus />
-        <LikesButton />
+    
+        {/* <UserStatus />
+        <LikesButton /> */}
+
+        <form className="container col-lg-4 offset-lg-4 mt-5"
+                onSubmit={handleSubmit}>
+          <fieldset>
+            <legend className="text-center">User Registration</legend>
+            
+            <div className="form-group">
+              <label for="exampleInputEmail1" className="form-label mt-4">Email address</label>
+              <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter email" fdprocessedid="cz21oq" />
+            </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1" className="form-label mt-4">Password</label>
+              <input type="password" className="form-control" id="userPassword" placeholder="Password" autocomplete="off" fdprocessedid="o2rlil" />
+            </div>
+      
+ 
+            <button type="submit" className="btn btn-primary mt-4" fdprocessedid="hnwth">Register</button>
+          </fieldset>
+        </form>
       </div> 
     );
   }
